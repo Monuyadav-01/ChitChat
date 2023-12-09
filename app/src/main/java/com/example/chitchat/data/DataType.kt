4698b9b914d999f70bdbdc89aa40ffc0f8cf4fc6
monu <monuyadav10012000@gmail.com>
2023-12-10 01:24:38 +0530

    commit push

--- app/src/main/java/com/example/chitchat/data/DataType.kt
+++ app/src/main/java/com/example/chitchat/data/DataType.kt
@@ -4,7 +4,9 @@ data class UserData(
     var userId: String? = "",
     var name: String? = "",
     var number: String? = "",
-    var imageUrl: String? = ""
+    var imageUrl: String? = "",
+    var email: String? ="",
+    var password : String?=""
 ) {
     fun toMap() = mapOf(
         "userId" to userId,
